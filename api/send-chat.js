1a5aec4b08a35dfeecabbabdb194366372677821
퍼돌이 <shenji323@gmail.com>
2026-03-11 22:04:35 +0900

    Update send-chat.js

--- api/send-chat.js
+++ api/send-chat.js
@@ -2,10 +2,11 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).send('Method Not Allowed');
 
     // 여기서 Vercel 대시보드에 적은 이름을 그대로 가져옵니다.
-    const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_CHAT_URL;
+    const WEBHOOK_MEDIA_URL = process.env.DISCORD_WEBHOOK_CHAT_URL;
+    const WEBHOOK_CHAT_URL = process.env.DISCORD_WEBHOOK_CHAT_URL;
 
     try {
-        await fetch(WEBHOOK_URL, {
+        await fetch(WEBHOOK_CHAT_URL, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(req.body) // 클라이언트가 보낸 데이터 그대로 전달
